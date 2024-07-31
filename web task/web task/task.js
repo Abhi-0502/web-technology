92fa03ad121951dff45f368631a96f152ecfabe9
Abhi-0502 <abhilashamahi0502@gmail.com>
2024-08-01 00:31:41 +0300

    task2.html

---
+++ web task/web task/task.js
@@ -0,0 +1,50 @@
+          
+         var dec = 1
+         let dec2 = 5
+         function clickMe(){
+            console.log('variables1', variables1);
+            var variables1 = 'here'
+            console.log('a 5>>>',a);
+            let a = 10
+            console.log('x>>>>',a);
+            console.log('x>>>',variables1)
+         }
+
+          let dec3 = 5
+          let obj ={'Iot':"84",'SAN':"74",'PWP2':"92",'TS':"99",'Internship':"99"}
+          let obj2 ={'kannada':"96",'English':"74",'Physics':"82",'Chemistry':"80",'Maths':"70",'Biology':"80"}
+          let obj3 ={'kannada':"124",'English':"96",'Hindi':"82",'SocialScience':"80",'Maths':"70",'Science':"80"}
+          let array1 =['Vivekananda Kannada And English Medium Bannur','Govt PU College Bannur','Cauvery Institute Technology Mandya']
+          let name = 'abhi'
+          let name2 = 'abhi'
+          let age = 22
+          let weight = 50
+          function clickMe(){
+            console.log(name2);
+            console.log(array1[2]);
+             console.log(obj.Iot);
+             console.log(obj.SAN);
+             console.log(obj.PWP2);
+             console.log(obj.TS);
+             console.log(obj.Internship);
+             console.log(array1[0]);
+             console.log(obj2.kannada);
+             console.log(obj2.English);
+             console.log(obj2.Physics);
+             console.log(obj2.Chemistry);
+             console.log(obj2.Maths);
+             console.log(obj2.Biology);
+             console.log(array1[1]);
+             console.log(obj3.kannada);
+             console.log(obj3.English);
+             console.log(obj3.Hindi);
+             console.log(obj3.SocialScience);
+             console.log(obj3.Maths);
+             console.log(obj3.Science);
+             console.log(age);
+             console.log(weight);
+             console.log(dec2);
+             var dec = 1
+             //let dec2 = 50
+             console.log(dec);
+          }
